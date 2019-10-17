de29185208cc8fb001b2762e58502ee7341130fc
Ping He <friedenhe@gmail.com>
2019-10-17 13:10:21 -0400

    Added the Ahmedbody 2D case.

---
+++ tutorials/AhmedBody2D/runROM/LHS_Sampling.py
@@ -0,0 +1,19 @@
+import matplotlib
+matplotlib.use('Agg') 
+import numpy as np
+import matplotlib.pyplot as plt
+
+from smt.sampling_methods import LHS
+
+xlimits = np.array([[0.0,0.05],[0.0, 0.05],[0.0,0.05],[0.0,0.05]])
+sampling = LHS(xlimits=xlimits,criterion='cm')
+
+num = 20
+x = sampling(num)
+
+print(x.shape)
+print(x)
+plt.plot(x[:, 0], x[:, 1], "o")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.savefig('LHS.png',bbox_inches='tight')   # save the figure to file
